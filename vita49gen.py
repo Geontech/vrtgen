e3eb311eaa2f148090979654fb7a851fcc9cdf4e
Justin A. Irwin <jirwin@geontech.com>
2019-02-13 09:50:13 -0500

    Improve command line interface with argument parser.

--- vita49gen.py
+++ vita49gen.py
@@ -1,21 +1,46 @@
 #!/usr/bin/python3
 
+__version__ = '0.1'
+
 import logging
+import argparse
 import sys
 
 from v49gen.parser import FileParser
 
 logging.basicConfig()
-logging.getLogger().setLevel(logging.DEBUG)
 
-def hex_bytes(data):
-    return ''.join('{:02x}'.format(ch) for ch in data)
+def group_bytes(data, size=8):
+    for pos in range(0, len(data), size):
+        yield data[pos:pos+size]
+
+def dump_bytes(data, stream):
+    offset = 0
+    for chunk in group_bytes(data):
+        data = ''.join('{:02x}'.format(ch) for ch in chunk)
+        stream.write('0x{0:04x} {1} {2}\n'.format(offset, data[:8], data[8:]))
+        offset += 0x10
 
 if __name__ == '__main__':
-    source = sys.argv[1]
-    p = FileParser()
-    for packet in p.parse(source):
-        prologue = packet.get_header_bytes()
-        logging.debug('Packet prologue 0x%s', hex_bytes(prologue))
-        if packet.has_trailer:
-            logging.debug('Packet trailer  0x%s', hex_bytes(packet.trailer.get_bytes()))
+    arg_parser = argparse.ArgumentParser(description='Generate VITA 49.2 packet classes.')
+    arg_parser.add_argument('filename', nargs='+', help='VRT YAML definition file')
+    arg_parser.add_argument('-v', '--verbose', action='store_true', default=False,
+                            help='display debug messages')
+    arg_parser.add_argument('--version', action='version', version='%(prog)s '+__version__)
+
+    args = arg_parser.parse_args()
+
+    if args.verbose:
+        logging.getLogger().setLevel(logging.DEBUG)
+
+    for filename in args.filename:
+        logging.debug('Parsing %s', filename)
+        for packet in FileParser().parse(filename):
+            prologue = packet.get_header_bytes()
+            if args.verbose:
+                print('Packet prologue:')
+                dump_bytes(prologue, sys.stdout)
+                if packet.has_trailer:
+                    trailer = packet.trailer.get_bytes()
+                    print('Packet trailer:')
+                    dump_bytes(trailer, sys.stdout)
