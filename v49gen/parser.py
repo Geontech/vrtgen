07c959e2e90612ad69c151eefaccd1ea5acca74c
Justin A. Irwin <jirwin@geontech.com>
2019-02-20 10:18:35 -0500

    Remove old parser code, make Class ID subfields top-level fields.

--- v49gen/parser.py
+++ v49gen/parser.py
@@ -6,201 +6,12 @@ import re
 
 import yaml
 
-class ParserError(RuntimeError):
-    def __init__(self, message, node):
-        super().__init__(message)
-        self.node = node
-
-    @property
-    def line(self):
-        return self.node.start_mark.line
-
-    @property
-    def column(self):
-        return self.node.start_mark.column
-
-class FieldParser:
-    __slots__ = ('name', 'func')
-
-    def __init__(self, name, func):
-        self.name = name
-        self.func = func
-
-    def parse_value(self, node):
-        return self.func(node)
-
-FieldValue = collections.namedtuple('FieldValue', ['value', 'optional'])
-
-class PacketParser:
-    def __init__(self, name):
-        self.name = name
-        self.log = logging.getLogger(name)
-        self.type = None
-        self.fields = {}
-        self.field_parsers = []
-        self.constructor = yaml.constructor.SafeConstructor()
-
-        for field in VRTPrologue().fields:
-            if field.name in ('TSI', 'TSF', 'Class ID'):
-                continue
-            self.add_field_parser(field.name, field.format)
-        self.add_field_parser('Class ID', func=self.parse_class_id)
-        self.add_field_parser('TSI', func=self.parse_tsi)
-        self.add_field_parser('TSF', func=self.parse_tsf)
-
-        for field in VRTDataTrailer().fields:
-            self.add_field_parser(field.name, field.format)
-
-        for name, _, format in CIF0.FIELDS:
-            self.add_field_parser(name, format)
-
-    def parse_class_id(self, node):
-        if not isinstance(node, yaml.MappingNode):
-            raise TypeError('structured data required')
-        value = {}
-        for key_node, value_node in node.value:
-            field_name = key_node.value.lower()
-            if field_name == 'oui':
-                value['OUI'] = self.parse_oui(value_node)
-            elif field_name == 'information class code':
-                value['Information Class Code'] = self.parse_int(value_node)
-            elif field_name == 'packet class code':
-                value['Packet Class Code'] = self.parse_int(value_node)
-            else:
-                self.warning("Invalid field '%s' for Class ID", key_node.value)
-        return value
-
-    def parse_oui(self, node):
-        return self.constructor.construct_yaml_str(node)
-
-    def parse_int(self, node):
-        return self.constructor.construct_yaml_int(node)
-
-    def parse_float(self, node):
-        return self.constructor.construct_yaml_float(node)
-
-    def parse_bit(self, node):
-        try:
-            self.constructor.construct_yaml_bool(node)
-        except:
-            return {'0':False, '1':True}[node.value]
-
-    def parse_empty(self, node):
-        raise ValueError('cannot set value')
-
-    def add_field_parser(self, name, format=None, func=None):
-        if not func:
-            if format == BIT:
-                func = self.parse_bit
-            elif isinstance(format, FixedFormat):
-                # TODO: bits/range
-                func = self.parse_float
-            elif isinstance(format, IntFormat):
-                # TODO: bit widths
-                func = self.parse_int
-            else:
-                func = self.parse_empty
-        self.field_parsers.append(FieldParser(name, func))
-
-    def get_field_parser(self, name):
-        for parser in self.field_parsers:
-            if name.lower() == parser.name.lower():
-                return parser
-        return None
-
-    def warning(self, *args, **kwargs):
-        self.log.warning(*args, **kwargs)
-
-    def error(self, *args, **kwargs):
-        self.log.error(*args, **kwargs)
-
-    def parse_field(self, name, value_node=None, optional=False):
-        if name in self.fields:
-            self.warning("Duplicate field '%s'", name)
-            return
-        field = self.get_field_parser(name)
-        if field is None:
-            self.error("Invalid field '%s'", name)
-            return
-        if value_node is not None:
-            try:
-                value = field.parse_value(value_node)
-            except (KeyError, ValueError) as exc:
-                self.error("Invalid value for field '%s': '%s'", field.name, value_node.value)
-                return
-            self.log.debug('Field %s has value %s', field.name, value)
-        else:
-            value = None
-        self.log.debug('Field %s is %s', field.name, 'optional' if optional else 'required')
-        self.fields[field.name] = FieldValue(value, optional)
-
-    def parse_packet_type(self, node):
-        packet_type = self.constructor.construct_yaml_str(node)
-        if packet_type.lower() == 'data':
-            self.type = VRTDataPacket
-        elif packet_type.lower() == 'context':
-            self.type = VRTContextPacket
-        elif packet_type.lower() == 'control':
-            self.type = VRTControlPacket
-        else:
-            raise ParserError("Invalid packet type '"+packet_type+"'")
-
-    def parse_field_list_entry(self, node):
-        if isinstance(node, yaml.ScalarNode):
-            return node.value, None
-        elif isinstance(node, yaml.MappingNode):
-            if len(node.value) != 1:
-                raise ParserError('Multiple field keys', node)
-            key_node, value_node = node.value[0]
-            return key_node.value, value_node
-        else:
-            raise ParserError('Invalid field description', node)
-
-    def parse_fields(self, node, optional=False):
-        if not isinstance(node, yaml.SequenceNode):
-            self.error('Packet fields must be sequence', node)
-            return
-
-        for field in node.value:
-            try:
-                field_name, value_node = self.parse_field_list_entry(field)
-            except ParserError as exc:
-                self.error('%s', exc)
-                continue
-            self.parse_field(field_name, value_node, optional)
-
-    def parse(self, node):
-        for key_node, value_node in MergingIterator(node.value):
-            field_name = key_node.value
-            if field_name.lower() in ('required', 'optional'):
-                # Within an attribute scope, set the attribute on all fields
-                optional = (field_name.lower() == 'optional')
-                self.parse_fields(value_node, optional)
-            elif field_name.lower() == 'type':
-                try:
-                    self.parse_packet_type(value_node)
-                except ParserError as exc:
-                    self.error('%s', exc)
-            else:
-                field = self.parse_field(field_name, value_node)
-                if field is None:
-                    continue
-
-        if self.type is None:
-            raise ParserError('No packet type provided', node)
-        packet = self.type(self.name)
-        for field_name, field_value in self.fields.items():
-            try:
-                field = packet.add_field(field_name, field_value.optional)
-            except KeyError as exc:
-                self.error("'%s' is not a valid field for packet type", field_name)
-                continue
-            if field_value.value is not None:
-                field.set_value(field_value.value)
-        return packet
-
 class FileParser:
     field_parsers = {}
+
+    @classmethod
+    def add_field_parser(cls, name, parser):
+        cls.field_parsers[name.casefold()] = parser
 
     def parse(self, filename):
         with open(filename, 'r') as fp:
@@ -213,7 +24,7 @@ class FileParser:
                 try:
                     yield self.parse_packet(name, value)
                 except Exception as exc:
-                    logging.error("%s", str(exc))
+                    logging.exception("%s %s", name, str(exc))
 
     def split_value(self, value):
         if isinstance(value, dict):
@@ -263,62 +74,65 @@ class FileParser:
 
         return packet
 
-    def parse_field_value(self, name, value):
-        key = name.casefold()
-        parser = self.field_parsers.get(key)
-        if parser:
-            return parser(self, value)
+    def parse_tsi(self, packet, value):
+        value = {'none':  TSI.NONE,
+                 'utc':   TSI.UTC,
+                 'gps':   TSI.GPS,
+                 'other': TSI.OTHER}[value.lower()]
+        packet.tsi.set_value(value)
+        packet.tsf.set_required()
+
+    def parse_tsf(self, packet, value):
+        value = {'none':         TSF.NONE,
+                 'samples':      TSF.SAMPLE_COUNT,
+                 'picoseconds':  TSF.REAL_TIME,
+                 'free running': TSF.FREE_RUNNING}[value.lower()]
+        packet.tsf.set_value(value)
+        packet.tsf.set_required()
+
+    def parse_class_id(self, packet, value):
+        if value is None:
+            return
+        elif str(value).casefold() == 'required':
+            packet.class_id.set_required()
+        elif str(value).casefold() == 'optional':
+            packet.class_id.set_optional()
         else:
-            return value
-
-    def parse_tsi(self, value):
-        return {'none':  TSI.NONE,
-                'utc':   TSI.UTC,
-                'gps':   TSI.GPS,
-                'other': TSI.OTHER}[value.lower()]
-    field_parsers['tsi'] = parse_tsi
-
-    def parse_tsf(self, value):
-        return {'none':         TSF.NONE,
-                'samples':      TSF.SAMPLE_COUNT,
-                'picoseconds':  TSF.REAL_TIME,
-                'free running': TSF.FREE_RUNNING}[value.lower()]
-    field_parsers['tsf'] = parse_tsf
+            raise ValueError(value)
 
     HEX_DIGIT = r'[0-9a-zA-Z]'
     OUI_RE = re.compile(r'({0})-({0})-({0})$'.format(HEX_DIGIT*2))
-    def parse_oui(self, value):
+    def parse_oui(self, packet, value):
         match = self.OUI_RE.match(value)
         if not match:
             raise ValueError('OUI format must be XX-XX-XX')
-        return int(''.join(match.groups()), 16)
-    field_parsers['oui'] = parse_oui
+        packet.class_id.oui = int(''.join(match.groups()), 16)
+        packet.class_id.set_required()
 
-    def parse_class_id(self, value):
-        if value is None:
-            return None
-        oui = None
-        information_class = None
-        packet_class = None
-        for field_name, field_value in value.items():
-            name = field_name.casefold()
-            if name == 'oui':
-                oui = self.parse_oui(field_value)
-            elif name == 'information class code':
-                information_class = int(field_value)
-            elif name == 'packet class code':
-                packet_class = int(field_value)
-            else:
-                logging.warn("Invalid Class ID field '%s'", field_name)
-        return (oui, information_class, packet_class)
-    field_parsers['class id'] = parse_class_id
+    def parse_information_class(self, packet, value):
+        packet.class_id.information_class = int(value)
+        packet.class_id.set_required()
+
+    def parse_packet_class(self, packet, value):
+        packet.class_id.packet_class = int(value)
+        packet.class_id.set_required()
 
     def parse_field(self, packet, name, value):
+        parser = self.field_parsers.get(name.casefold(), None)
+        if parser is not None:
+            return parser(self, packet, value)
+
         try:
             field = packet.add_field(name)
         except KeyError:
             logging.error("Invalid field '%s' for packet %s", name, packet.name)
             return
-        value = self.parse_field_value(name, value)
         logging.debug("Field '%s' = %s", name, value)
         field.set_value(value)
+
+FileParser.add_field_parser('TSI', FileParser.parse_tsi)
+FileParser.add_field_parser('TSF', FileParser.parse_tsf)
+FileParser.add_field_parser('Class ID', FileParser.parse_class_id)
+FileParser.add_field_parser('OUI', FileParser.parse_oui)
+FileParser.add_field_parser('Packet Class Code', FileParser.parse_packet_class)
+FileParser.add_field_parser('Information Class Code', FileParser.parse_information_class)
