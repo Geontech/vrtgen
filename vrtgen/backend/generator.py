758dc9805ade5c453b3395b976ea8a4887e75482
Justin A. Irwin <jirwin@geontech.com>
2019-04-19 15:50:36 -0400

    Add backend option support.

--- vrtgen/backend/generator.py
+++ vrtgen/backend/generator.py
@@ -1,3 +1,6 @@
 class Generator:
+    def set_option(self, name, value):
+        raise ValueError("invalid option '"+name+"'")
+
     def generate(self, packet):
         raise NotImplementedError('generate')
