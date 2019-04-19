5956730b7362d9587838f12c679e8741e197bd4c
Justin A. Irwin <jirwin@geontech.com>
2019-04-19 11:28:30 -0400

    Rename executable and add initial backend support.

---
+++ vrtgen/backend/bindump.py
@@ -0,0 +1,22 @@
+import sys
+
+def group_bytes(data, size=8):
+    for pos in range(0, len(data), size):
+        yield data[pos:pos+size]
+
+def dump_bytes(data, stream):
+    offset = 0
+    for chunk in group_bytes(data):
+        data = ''.join('{:02x}'.format(ch) for ch in chunk)
+        stream.write('0x{0:04x} {1} {2}\n'.format(offset, data[:8], data[8:]))
+        offset += 0x10
+
+class BinaryDumper:
+    def generate(self, packet):
+        print('Packet ' + packet.name)
+        prologue = packet.get_prologue_bytes()
+        dump_bytes(prologue, sys.stdout)
+        if packet.has_trailer:
+            trailer = packet.trailer.get_bytes()
+            print('Trailer:')
+            dump_bytes(trailer, sys.stdout)
