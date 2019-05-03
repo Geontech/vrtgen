d5df5ba60a7e2c9312f08a61f595e2bcb2c0f3c8
Justin A. Irwin <jirwin@geontech.com>
2019-05-03 13:19:07 -0400

    Make CIF fields instance iterable instead of requiring a method call.

--- vrtgen/types/cifmeta.py
+++ vrtgen/types/cifmeta.py
@@ -3,7 +3,7 @@ from .struct import Struct, StructEntry, Enable, Field, Reserved
 class ContextIndicatorFields:
     def __init__(self, name):
         self.name = name
-        self._fields = []
+        self._contents = []
 
     def __setattr__(self, attr, value):
         # Automatically add struct entries to the internally-tracked list of
@@ -15,18 +15,21 @@ class ContextIndicatorFields:
         # This pattern supports defining fields as attributes for more static
         # checking of names.
         if isinstance(value, StructEntry) and not hasattr(self, attr):
-            self._fields.append((attr, value))
+            # Save the attribute name for use in generating the enable struct
+            self._contents.append((attr, value))
         return super().__setattr__(attr, value)
 
-    def get_fields(self):
+    def __iter__(self):
         """
-        Returns the CIF fields that can be present in a Context/Command packet
-        payload.
+        Returns an iterator over the CIF fields that can be present in a
+        Context/Command packet payload.
         """
-        # Only return contents that are defined as fields. Some CIF0 bits are
-        # simple indicators, and there are a handful of reserved bits; don't
-        # report these.
-        return [value for attr, value in self._fields if isinstance(value, Field)]
+        for attr, value in self._contents:
+            # Only return contents that are defined as fields. Some CIF0 bits
+            # are simple indicators, and there are a handful of reserved bits;
+            # don't report these.
+            if isinstance(value, Field):
+                yield value
 
     def create_enables(self):
         """
@@ -34,7 +37,7 @@ class ContextIndicatorFields:
         defined fields.
         """
         namespace = {}
-        for attr, field in self._fields:
+        for attr, field in self._contents:
             # Turn all non-reserved CIF bits into enable flags
             if isinstance(field, Reserved):
                 entry = Reserved(1)
