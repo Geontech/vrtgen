7873bf7c96ae412ad7e7fe4882a02477fac9ee47
Justin A. Irwin <jirwin@geontech.com>
2019-05-03 11:42:04 -0400

    Refine struct metaclass to rely on less local state.

--- vrtgen/types/struct.py
+++ vrtgen/types/struct.py
@@ -39,7 +39,7 @@ class Field(StructEntry):
     def __set__(self, instance, value):
         if not isinstance(value, self.type):
             value = self.type(value)
-        setattr(instance, self._attr, value)        
+        setattr(instance, self._attr, value)
 
     @property
     def bits(self):
@@ -48,45 +48,41 @@ class Field(StructEntry):
 class StructBuilder(type):
     def __init__(cls, name, bases, namespace):
         type.__init__(cls, name, bases, namespace)
-        cls._contents = []
-        word = 0
-        offset = 31
-        size = 0
         for attr, value in namespace.items():
             if not isinstance(value, StructEntry):
                 continue
-            if not StructBuilder._check_alignment(offset, value):
-                msg = '{}.{} is not naturally aligned'.format(name, attr)
-                warnings.warn(msg)
-            cls._contents.append(value)
-            value.word = word
-            value.offset = offset
-            size += value.bits
-            offset -= value.bits
-            if offset < 0:
-                word += 1
-                offset += 32
-        cls.bits = size
+            cls.add_field(attr, value)
+        cls.check_size()
 
-    @classmethod
-    def _check_alignment(cls, offset, field):
+    def add_field(cls, attr, field):
+        word = cls.bits // 32
+        offset = 31 - (cls.bits % 32)
+        if not cls.check_alignment(offset, field.bits):
+            msg = '{}.{} is not naturally aligned'.format(cls.__name__, attr)
+            warnings.warn(msg)
+        cls._contents.append(field)
+        field.word = word
+        field.offset = offset
+        cls.bits += field.bits
+
+    def check_alignment(cls, offset, bits):
         """
         Returns true if a field  is naturally aligned within a struct based on
         its starting offset.
         """
         # Adjust from 0-based to 1-based indexing to determine alignment
         pos = offset + 1
-        if field.bits < 8:
+        if bits < 8:
             # No alignment concerns for sub-byte fields
             return True
-        elif field.bits in (8, 16, 32):
+        elif bits in (8, 16, 32):
             # Common power-of-two integer sizes should be naturally aligned
-            return (pos % field.bits) == 0
-        elif field.bits == 24:
+            return (pos % bits) == 0
+        elif bits == 24:
             # 24-bit integers just need to be 8-bit aligned, presumably since
             # they are not a "natural" register size
             return (pos % 8) == 0
-        elif field.bits == 64:
+        elif bits == 64:
             # 64-bit ints must be at least aligned to a 32-bit word boundary,
             # but full 64-bit alignment does not appear to be a concern
             return pos == 32
@@ -94,7 +90,24 @@ class StructBuilder(type):
             # Other cases TBD
             return True
 
+    def check_size(cls):
+        if cls.bits % 32:
+            msg = '{} does not end on a word boundary'.format(cls.__name__)
+            warnings.warn(msg)
+
 class Struct(metaclass=StructBuilder):
+    # Initialize size to 0, subclasses will update their own value
+    bits = 0
+
+    # Initialize contents to empty, subclasses will extend
+    _contents = []
+
+    def __init_subclass__(cls, *args, **kwds):
+        super().__init_subclass__(*args, **kwds)
+        # Copy the contents list so that subclasses do not accidentally modify
+        # base class contents
+        cls._contents = cls._contents[:]
+
     @classmethod
     def get_contents(cls):
         return cls._contents
