cab1b7429c79b4337510926f2abf6730729b711d
Justin A. Irwin <jirwin@geontech.com>
2019-05-13 15:14:07 -0400

    Add docstrings to field configuration classes.
    Fixed CI issue where the Python dependencies were not installed in the 
    lint job.

--- vrtgen/model/field.py
+++ vrtgen/model/field.py
@@ -1,15 +1,30 @@
+"""
+Types for configuration of packet fields and subfields.
+"""
+
 from enum import IntEnum
 import inspect
 
 from vrtgen.types.struct import Field, Struct
 
 class Mode(IntEnum):
+    """
+    Defines the usage of a field or subfield:
+        DISABLED: Field is never present.
+        OPTIONAL: Field can be enabled or disabled at runtime.
+        REQUIRED: Field is always present.
+        MANDATORY: Field is present and cannot and be disabled or optional.
+    """
     DISABLED = 0
     OPTIONAL = 1
     REQUIRED = 2
     MANDATORY = 3
 
 class FieldConfiguration:
+    """
+    Base class to represent the packet-specific configuration of a particular
+    field or subfield.
+    """
     def __init__(self, field, mode=Mode.DISABLED):
         self.field = field
         self._mode = mode
@@ -17,14 +32,25 @@ class FieldConfiguration:
 
     @property
     def name(self):
+        """
+        VITA 49.2 field or subfield name.
+        """
         return self.field.name
 
     @property
     def type(self):
+        """
+        Data type of the field.
+        """
         return self.field.type
 
     @property
     def mode(self):
+        """
+        The configured usage of this field.
+
+        See Mode.
+        """
         return self._mode
 
     @mode.setter
@@ -39,45 +65,77 @@ class FieldConfiguration:
 
     @property
     def is_enabled(self):
+        """
+        True if this field may be present in a packet.
+        """
         return self._mode != Mode.DISABLED
 
     @property
     def is_set(self):
+        """
+        True if this field is required, or is optional and has a value.
+        """
         if self.has_value:
             return True
         return self.is_mandatory
 
     @property
     def is_required(self):
+        """
+        True if this field must be present in the containing packet.
+        """
         return self._mode in (Mode.REQUIRED, Mode.MANDATORY)
 
     @property
     def is_optional(self):
+        """
+        True if this field can be enabled or disabled at runtime.
+        """
         return self._mode == Mode.OPTIONAL
 
     @property
     def is_mandatory(self):
+        """
+        True if this field must be present for all packets and cannot be
+        disabled.
+        """
         return self._mode == Mode.MANDATORY
 
     @property
     def is_disabled(self):
+        """
+        True if this field is not present in the containing packet.
+        """
         return self._mode == Mode.DISABLED
 
     @property
     def is_constant(self):
+        """
+        True if this field's value is immutable.
+        """
         return self._constant
 
     def set_constant(self):
+        """
+        Set or clear the constant status of this field's value.
+        """
         if not self.is_required:
             self._mode = Mode.REQUIRED
         self._constant = True
 
     @property
     def has_value(self):
+        """
+        True if a value has been explicitly set for this field.
+        """
         raise NotImplementedError('has_value')
 
     @staticmethod
     def create(field, mode=Mode.DISABLED):
+        """
+        Creates the correct subclass of FieldConfiguration for a given field
+        instance.
+        """
         # The type may be none for unimplemented CIF fields
         if field.type is not None and issubclass(field.type, Struct):
             return StructFieldConfiguration(field, mode=mode)
@@ -85,6 +143,9 @@ class FieldConfiguration:
             return SimpleFieldConfiguration(field, mode=mode)
 
 class SimpleFieldConfiguration(FieldConfiguration):
+    """
+    Configuration for a simple field type (integer, fixed-point, etc.).
+    """
     def __init__(self, field, mode=Mode.DISABLED):
         super().__init__(field, mode)
         self.value = None
@@ -94,6 +155,9 @@ class SimpleFieldConfiguration(FieldConfiguration):
         return self.value is not None
 
 class StructFieldConfiguration(FieldConfiguration):
+    """
+    Configuration for a structured field type.
+    """
     def __init__(self, field, mode=Mode.DISABLED):
         super().__init__(field, mode)
         self._fields = {}
@@ -105,6 +169,9 @@ class StructFieldConfiguration(FieldConfiguration):
             self._fields[subfield.name] = subfield
 
     def get_field(self, name):
+        """
+        Returns the configuration for a subfield by VITA 49.2 name.
+        """
         return self._fields.get(name, None)
 
     @property
