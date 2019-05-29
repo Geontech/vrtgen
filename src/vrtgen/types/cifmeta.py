5d450584b7df25cb7d68c953238b44f2c7cba378
Justin A. Irwin <jirwin@geontech.com>
2019-05-29 15:29:44 -0400

    Change the naming of CIF enable structs to include the CIF struct name.

--- src/vrtgen/types/cifmeta.py
+++ src/vrtgen/types/cifmeta.py
@@ -11,13 +11,14 @@ class CIFMeta(type):
     Metaclass for CIF fields.
     """
     def __new__(cls, name, bases, namespace):
-        cif = type.__new__(cls, name, bases, namespace)
-        cif.Enables = CIFMeta._create_enables(namespace)
-        return cif
+        namespace['Enables'] = CIFMeta.create_enables(name, namespace)
+        return type.__new__(cls, name, bases, namespace)
 
     @staticmethod
-    def _create_enables(cifdict):
-        # Dynamically create a struct class for the CIF prologue structure
+    def create_enables(name, cifdict):
+        """
+        Dynamically creates a struct class for the CIF prologue structure.
+        """
         namespace = {}
         for attr, field in cifdict.items():
             if not isinstance(field, StructEntry):
@@ -28,4 +29,15 @@ class CIFMeta(type):
             else:
                 entry = Field(field.name, basic.Boolean)
             namespace[attr] = entry
-        return StructBuilder('Enables', (Struct,), namespace)
+
+        # Pass the qualified name (which includes the containing class) to
+        # StructBuilder and then remove it later. This helps solve two
+        # problems:
+        #   * If the struct is incorrectly sized, the warning message will
+        #     indicate which CIF struct it's complaining about.
+        #   * The qualified name does not appear to be set when dynamically
+        #     creating a class
+        qualname = name+'.Enables'
+        cls = StructBuilder(qualname, (Struct,), namespace)
+        cls.__name__ = 'Enables'
+        return cls
