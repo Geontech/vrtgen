84673caf8af67c47db843d1bcee26b160e333313
Justin A. Irwin <jirwin@geontech.com>
2019-06-10 12:48:05 -0400

    Use C++11 for testing, and std::array for better test output on checks.

--- cpp/tests/test.cpp
+++ cpp/tests/test.cpp
@@ -7,14 +7,30 @@
 #include <vrtgen/packing/header.hpp>
 #include <vrtgen/packing/trailer.hpp>
 
+namespace Catch {
+    template <size_t N>
+    struct StringMaker< std::array<uint8_t, N> >
+    {
+        static std::string convert(const std::array<uint8_t, N>& value)
+        {
+            std::ostringstream oss;
+            oss << "0x" << std::hex << std::setfill('0') << std::setiosflags(std::ios::uppercase);
+            for (auto item : value) {
+                oss << std::setw(2) << static_cast<unsigned>(item);
+            }
+            return oss.str();
+        }
+    };
+}
+
 TEST_CASE("Header getter methods")
 {
     using vrtgen::packing::Header;
 
-    uint8_t data[] = {
+    std::array<uint8_t,4> data = {
         0x10, 0x5c, 0x12, 0x34,
     };
-    Header& header = *reinterpret_cast<Header*>(data);
+    Header& header = *reinterpret_cast<Header*>(data.data());
     SECTION("Packet Type") {
         REQUIRE(header.getPacketType() == vrtgen::PacketType::SIGNAL_DATA_STREAM_ID);
     }
@@ -39,33 +55,39 @@ TEST_CASE("Header setter methods")
 {
     using vrtgen::packing::Header;
 
-    uint8_t data[] = { 0, 0, 0, 0 };
-    Header& header = *reinterpret_cast<Header*>(data);
+    std::array<uint8_t,4> data = {}; // zero-initialize
+    std::array<uint8_t,4> expected;
+    Header& header = *reinterpret_cast<Header*>(data.data());
 
     SECTION("Packet Type") {
+        expected = { 0x40, 0x00, 0x00, 0x00 };
         header.setPacketType(vrtgen::PacketType::CONTEXT);
-        REQUIRE(data[0] == 0x40);
+        REQUIRE(data == expected);
     }
     SECTION("Class Identifier Enable") {
+        expected = { 0x08, 0x00, 0x00, 0x00 };
         header.setClassIdentifierEnabled(true);
-        REQUIRE(data[0] == 0x08);
+        REQUIRE(data == expected);
     }
     SECTION("TSI") {
+        expected = { 0x00, 0x80, 0x00, 0x00 };
         header.setTSI(vrtgen::TSI::GPS);
-        REQUIRE(data[1] == 0x80);
+        REQUIRE(data == expected);
     }
     SECTION("TSF") {
+        expected = { 0x00, 0x20, 0x00, 0x00 };
         header.setTSF(vrtgen::TSF::REAL_TIME);
-        REQUIRE(data[1] == 0x20);
+        REQUIRE(data == expected);
     }
     SECTION("Packet Count") {
+        expected = { 0x00, 0x0C, 0x00, 0x00 };
         header.setPacketCount(12);
-        REQUIRE(data[1] == 0x0C);
+        REQUIRE(data == expected);
     }
     SECTION("Packet Size") {
+        expected = { 0x00, 0x00, 0xCD, 0xEF };
         header.setPacketSize(0xCDEF);
-        REQUIRE(data[2] == 0xCD);
-        REQUIRE(data[3] == 0xEF);
+        REQUIRE(data == expected);
     }
 }
 
